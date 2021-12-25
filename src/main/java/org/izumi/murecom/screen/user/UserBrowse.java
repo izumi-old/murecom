7d97135d6c613dbcd6c613795a5a431df703f441
Aiden Izumi <aiden.izumi@gmail.com>
2021-12-25 07:00:08 +0000

    Initial commit

---
+++ src/main/java/org/izumi/murecom/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package org.izumi.murecom.screen.user;
+
+import org.izumi.murecom.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("murecom_User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
---
+++ src/test/java/org/izumi/murecom/MurecomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.izumi.murecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
